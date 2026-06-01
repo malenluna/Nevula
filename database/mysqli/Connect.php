c8dca49595dbdbec5fe062518b780be010220d18
MarielSzewaga <marieszewaga.com>
2026-06-01 18:32:26 -0300

    Conectamos la Base De Datos

--- database/mysqli/Connect.php
+++ database/mysqli/Connect.php
@@ -1,1 +1,11 @@
+<?php
 
+try {
+    $config = "mysql:host=localhost;dbname=nevula";
+    $conexion = new PDO($config, "root", "");
+}
+catch (PDOException $error) {
+    print $error->getMessage();
+}
+
+?>
