6a2aba28a179a4ff208297fa4d7c2a0cdc59ac28
jpalomino1 <juanapalomino1702@gmail.com>
2026-06-22 19:08:52 -0300

    Update Connect.php

--- database/mysqli/Connect.php
+++ database/mysqli/Connect.php
@@ -3,18 +3,11 @@
 try {
 
     $config = "mysql:host=srv1659.hstgr.io;dbname=u214138677_nevula;charset=utf8";
-
     $conexion = new PDO(
         $config,
         "u214138677_nevula",
         "YajuLuna26"
     );
-
-    echo "CONEXION OK";
-    exit;
-
 } catch (PDOException $error) {
-
-    die("ERROR MYSQL: " . $error->getMessage());
-
+    print $error->getMessage();
 }
