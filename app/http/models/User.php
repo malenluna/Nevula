a34f14c15de35b896f94846b50bb35d6c4d419be
jpalomino1 <juanapalomino1702@gmail.com>
2026-06-22 18:58:28 -0300

    Fix SQL query in buscarUsuario

--- app/http/models/User.php
+++ app/http/models/User.php
@@ -4,7 +4,7 @@ require_once __DIR__ . "/../../../database/mysqli/Connect.php";
 function buscarUsuario($usuario, $contrasena)
 {
     global $conexion;
-    $consulta = $conexion->prepare("SELECT * FROM usuarios WHERE Usuario = :usuario AND Contrasena = :contrasena"
+    $consulta = $conexion->prepare("SELECT * FROM Usuario WHERE Usuario = :usuario AND Contrasena = :contrasena"
     );
     $consulta->bindValue(':usuario', $usuario);
     $consulta->bindValue(':contrasena', $contrasena);
