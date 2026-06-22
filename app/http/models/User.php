251ef329dbaa96c1ae0f4cdc57c13eb836c770ec
jpalomino1 <juanapalomino1702@gmail.com>
2026-06-22 19:24:57 -0300

    Fix PHP syntax for User model file

--- app/http/models/User.php
+++ app/http/models/User.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ "./../../../database/mysqli/Connect.php";
+require_once __DIR__ . "/../../../database/mysqli/Connect.php";
  
 function buscarUsuario($usuario, $contrasena)
 {
