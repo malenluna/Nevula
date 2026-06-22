30f9259711220bc306d2a0f7b584792ff3618cba
jpalomino1 <juanapalomino1702@gmail.com>
2026-06-22 18:59:39 -0300

    Enhance MySQL connection with charset and error messages
    Added character set to PDO connection and improved error handling.

--- database/mysqli/Connect.php
+++ database/mysqli/Connect.php
@@ -1,14 +1,20 @@
 <?php
 
 try {
-    $config = "mysql:host=srv1659.hstgr.io;dbname=u214138677_nevula";
+
+    $config = "mysql:host=srv1659.hstgr.io;dbname=u214138677_nevula;charset=utf8";
+
     $conexion = new PDO(
         $config,
         "u214138677_nevula",
         "Miri2026"
     );
+
+    echo "CONEXION OK";
+    exit;
+
+} catch (PDOException $error) {
+
+    die("ERROR MYSQL: " . $error->getMessage());
+
 }
-catch (PDOException $error) {
-    print $error->getMessage();
-}
-?>
