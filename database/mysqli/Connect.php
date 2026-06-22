734c0637faccbeb4512308fdeead48ee4c5e9cca
jpalomino1 <juanapalomino1702@gmail.com>
2026-06-22 18:56:22 -0300

    se agrego configuracion de base de datos del hostinger

--- database/mysqli/Connect.php
+++ database/mysqli/Connect.php
@@ -2,7 +2,11 @@
 
 try {
     $config = "mysql:host=srv1659.hstgr.io;dbname=u214138677_nevula";
-    $conexion = new PDO($config, "root", "");
+    $conexion = new PDO(
+        $config,
+        "u214138677_nevula",
+        "Miri2026"
+    );
 }
 catch (PDOException $error) {
     print $error->getMessage();
