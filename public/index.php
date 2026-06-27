b9d273c2d7db4dafc26819ce1fe6206facfb6275
Agustin Yapura <agusyapura001@gmail.com>
2026-06-27 15:38:18 -0300

    prueba

--- public/index.php
+++ public/index.php
@@ -1,6 +1,4 @@
 <?php
-
-<?php
 if ($_SERVER['HTTP_HOST'] === 'localhost') {
     define('BASE_URL', '/Nevula-main/public');
 } else {
@@ -10,14 +8,11 @@ if ($_SERVER['HTTP_HOST'] === 'localhost') {
 require_once __DIR__ . "/../assets/tpl_engine/tpl_engine.php";
 
 if (isset($_GET['slug'])) {
-
     $slug = $_GET['slug'];
-
     $controllerPath = __DIR__ . "/../app/http/controllers/" . $slug . ".php";
     if (!file_exists($controllerPath)) {
         $controllerPath = __DIR__ . "/../app/http/controllers/NotFoundController.php";
     }
-
 } else {
     $controllerPath = __DIR__ . "/../app/http/controllers/LandingController.php";
 }
