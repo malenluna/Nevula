ae66a66a1b971c55aaddcdbe43181c2840eedd8f
agusyapura <agusyapura001@gmail.com>
2026-06-27 15:10:34 -0300

    prueba | intento corrección error en el header

--- app/http/controllers/LoginController.php
+++ app/http/controllers/LoginController.php
@@ -12,11 +12,11 @@ if ($resultado) {
     $_SESSION['id'] = $resultado['id_usuario'];
     $_SESSION['usuario'] = $resultado['usuario'];
     $_SESSION['rol'] = $resultado['rol'];
-    header("Location:  /public/index.php?slug=IndexController");
+    header("Location: /public/index.php?slug=IndexController");
 
     exit;
 } else {r
-    header("Location:  /public/index.php?slug=AuthController");
+    header("Location: /public/index.php?slug=AuthController");
     exit;
 }
 ?>
